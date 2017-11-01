052c202e94138385e5cc583cc6179b7b7a7405c2
Lucas Gulen <lucas.gulen@hotmail.ch>
2017-11-01 20:58:22 +0100

    Stable version

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './Equipe.js';
+import Equipe from './Equipe.js';
 
 class App extends React.Component {
  render(){
    return (
-     <div>
       <Equipe />
-      </div>
   );
  }
 }
