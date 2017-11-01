63a69a2c93df0ea78d716e9f497c8082c070bed3
Barreiro Lindo Flavio <flavio.barreiro-lindo@etu.hesge.ch>
2017-11-01 21:35:32 +0100

    Added icon left menu

--- front/src/App.js
+++ front/src/App.js
@@ -15,9 +15,10 @@ class App extends Component {
       
         <LeftMenu />
         <main id="page-wrap">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="app-header">
+          <div className="app-title">Reactive Football
+          <img className="user-photo" src="https://vignette.wikia.nocookie.net/mrbean/images/4/4b/Mr_beans_holiday_ver2.jpg/revision/latest/scale-to-width-down/250?cb=20100424114324" />
+          </div>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- front/src/components/leftMenu/LeftMenu.js
+++ front/src/components/leftMenu/LeftMenu.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { slide as Menu } from 'react-burger-menu';
-
+import FaFutbolO from 'react-icons/lib/fa/futbol-o';
 import './LeftMenu.css';
 
 class LeftMenu extends Component {
@@ -11,9 +11,10 @@ class LeftMenu extends Component {
 
   render() {
 //    <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-    
     return (
         <Menu>
+          <h2 className="menu-title"><span>Reactive</span>
+          <FaFutbolO /></h2>
             <a id="home" className="menu-item" href="/">Accueil</a>
             <a id="about" className="menu-item" href="/equipe">Equipe</a>
             <a id="contact" className="menu-item" href="/matchs">Matchs</a>
