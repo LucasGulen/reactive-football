a85ce09ef167b2c1d0986152f43093bad10b5217
Barreiro Lindo Flavio <flavio.barreiro-lindo@etu.hesge.ch>
2017-12-20 10:27:36 +0100

    Recherche implementée

--- reactive-football/src/views/CreationEquipe/CreationEquipe.js
+++ reactive-football/src/views/CreationEquipe/CreationEquipe.js
@@ -18,24 +18,33 @@ class CreationEquipe extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {recherche : ""};
+    this.handleChange = this.handleChange.bind(this);
+    this.executer = this.executer.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({recherche : event.target.value});
   }
   
   render() {
     return (
       <div className="animated fadeIn">
+        <input type="text" onChange={this.handleChange}/>
         <button onClick={() => {
             this.executer();
           }
-        } />
+          
+        }>Rechercher</button>
         <TableJoueurs onRef={ref => (this.tableJoueurs= ref)}/>
       </div>
     )
   }
 
   executer() {
-    getJoueursFilter("Ronal")
+    getJoueursFilter(this.state.recherche)
     .then((res) => {
-        this.tableJoueurs.populateTable(res.data);
+        this.tableJoueurs.populateTable(res.data === "" ? [] : res.data);
       }
     );
   }
