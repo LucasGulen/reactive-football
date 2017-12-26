8cdf2185abc0259af5d72b3ed09ec9b010f67997
Lucas Gulen <lucas.gulen@hotmail.ch>
2017-12-26 20:06:13 +0100

    Données en static

--- reactive-football/src/views/General/General.js
+++ reactive-football/src/views/General/General.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import Chart from './Chart';
+import Widget from './Widget';
+import Donut from './Donut';
+import {
+  Row,
+  Col,
+} from 'reactstrap';
 
 class General extends Component {
 
   render() {
     return (
       <div className="animated fadeIn">
-        Ceci est la view des statistiques generales
+        <Row>
+          <Col xs="6" >
+            <Donut/>
+          </Col>
+          <Col xs="6" >
+            <Chart/>
+          </Col >
+          <Col xs="6" >
+            <Widget/>
+          </Col >
+          <Col xs="6" >
+            <Widget/>
+          </Col >
+        </Row>
       </div>
     )
   }
